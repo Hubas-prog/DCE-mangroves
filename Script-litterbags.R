b288c4a3ab3fa674e284ac32dfa960ebcc8e4bc3
Hubas-prog <cedrichubas@mageos.com>
2020-11-23 14:30:01 +0100

    full analysis pigment

--- Script-litterbags.R
+++ Script-litterbags.R
@@ -27,7 +27,7 @@ my.palette <- colorRampPalette(c("red3","orange","yellow","green3","royalblue"))
 # ACP pigments
 ###################################
 
-names(PERCENT)
+names(percent)
 res.pca <- dudi.pca(PERCENT,scannf=F,nf=3)
 
 res.acp.var <- get_pca_var(res.pca) # obtenir les cos2/corel/contrib des variables
@@ -53,26 +53,54 @@ plot_grid(IND,VAR,labels=c("a","b"),ncol=1)
 ###################################
 
 res.bca <- bca(res.pca,fac,scannf=F,nf=3)
-
-ggplot(res.bca$ls,
+res.bca$ratio
+IND.bca <- ggplot(res.bca$ls,
   aes(x=CS1,y=CS2,col=fac))+
   geom_point(size=3)+
   stat_ellipse()+
-  theme_bw()
+  theme_linedraw(base_size = 14)
 
-  
-  
-  
-  
-  
+VAR.bca <- ggplot(res.bca$co,aes(x=Comp1,y=Comp2,label=rownames(res.bca$co)))+
+  geom_text(col="red")+
+  geom_segment(aes(x=0,y=0,xend=Comp1,yend=Comp2),
+               arrow = arrow(length = unit(0.5, "cm")),
+               col="grey")+
+  theme_linedraw(base_size = 14)
+
+plot_grid(IND.bca,VAR.bca,labels=c("a","b"),ncol=1)
+
+
 
 ###################################
 # PCoA pigments
 ###################################
 
-res.pcoa <- dudi.pco(cailliez(vegdist(PERCENT,"bray")),scannf=F,nf=3)
+res.pcoa <- dudi.pco(cailliez(vegdist(PERCENT,"bray")),
+                     scannf=F,
+                     nf=3)
+
+
+ggplot(res.pcoa$li,
+       aes(x=A1,y=A2,col=fac))+
+  geom_point(size=3)+
+  stat_ellipse()+
+  theme_linedraw(base_size = 14)
+
+
+###################################
+# nMDS pigments
+###################################
+
+res.nmds <- metaMDS(PERCENT,"bray",k=2)
+summary(res.nmds)
+
+ggplot(data.frame(res.nmds$points),
+       aes(x=MDS1,y=MDS2,col=fac))+
+  geom_point(size=3)+
+  stat_ellipse()+
+  theme_linedraw(base_size = 14)
 
 
 
-plot_grid(IND,VAR,labels=c("a","b"),ncol=1)
+
 
